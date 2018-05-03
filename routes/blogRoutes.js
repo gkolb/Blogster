5e1069f8b5d5e49ab1300de2d1dd88bf56e7ed1c
gkolb <gkolb100@gmail.com>
2018-05-03 15:58:17 -0600

    Create redis server for caching user blogs

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,6 +5,7 @@ const Blog = mongoose.model('Blog');
 
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
@@ -14,9 +15,25 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    const util = require('util');
+    client.get = util.promisify(client.get);
+
+    const cachedBlogs = await client.get(req.user.id);
+
+    if(cachedBlogs) {
+      console.log('SERVING FROM CACHE');
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
     const blogs = await Blog.find({ _user: req.user.id });
 
+    console.log('SERVING FROM MLABS');
     res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
