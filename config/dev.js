3f4954e4f9ea588f75fef0a0b89bc33efc1a82ad
gkolb <gkolb100@gmail.com>
2018-05-03 14:11:09 -0600

    Update dev database

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://gkolb:123@ds113358.mlab.com:13358/blog_dev_node',
   cookieKey: '123123123'
 };
