7b2a99ca5c60e59d395d5e13612aa3789513743e
gkolb <gkolb100@gmail.com>
2018-05-05 15:21:25 -0600

    Refactor test file by creating factories and setup.js

--- tests/factories/sessionFactory.js
+++ tests/factories/sessionFactory.js
@@ -3,17 +3,14 @@ const Keygrip = require('keygrip');
 const keys = require('../../config/keys');
 const keygrip = new Keygrip([keys.cookieKey]);
 
-
-module.export = (user) => {
+module.exports = user => {
   const sessionObject = {
     passport: {
       user: user._id.toString()
     }
   };
-  const session = Buffer.from(
-    JSON.stringify(sessionObject)
-  ).toString('base64');
+  const session = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
   const sig = keygrip.sign('session=' + session);
 
-  return { session, sig};
-}
+  return { session, sig };
+};
