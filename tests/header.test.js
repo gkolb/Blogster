9081af7c449a57f72fce994e7838b432b0f39993
gkolb <gkolb100@gmail.com>
2018-05-05 14:48:50 -0600

    Add test to check login by creating fake sessions

--- tests/header.test.js
+++ tests/header.test.js
@@ -24,3 +24,31 @@ test('clicking login starts oauth flow', async () => {
   const url = await page.url();
   expect(url).toMatch('/accounts\.google\.com/');
 })
+
+test('When signed in, shows logout button', async () => {
+  const id = '5aeb659c90055f2ca83778e4';
+
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+  const sessionString = Buffer.from(
+    JSON.stringify(sessionObject)
+  ).toString('base64');
+
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+  
+  await page.setCookie({ name: 'session', value: sessionString});
+  await page.setCookie({ name: 'session.sig', value: sig});
+  await page.goto('localhost:3000');
+  await page.waitFor('a[href="/auth/logout"]');
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+  expect(text).toEqual
+});
