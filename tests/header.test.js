29e8412b83ab157cb3f06ebd9518dad6c52ec277
gkolb <gkolb100@gmail.com>
2018-05-04 20:53:01 -0600

    Add beforeEach and afterEach hooks to open and close browsers

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,15 +1,21 @@
 const puppeteer = require('puppeteer');
 
-test('Add two numbers', () => {
-  const sum = 1 + 2
+let browswer, page;
 
-  expect(sum).toEqual(3);
-});
-
-test('We can launch a browser', async () => {
-  const browser = await puppeteer.launch({
+beforeEach(async() => {
+  browser = await puppeteer.launch({
     headless: false
   });
-  const page = await browser.newPage();
+  page = await browser.newPage();
   await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await browser.close();
+})
+
+test('We can launch a browser', async () => {
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
 });
