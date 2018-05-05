2067bcd0acfe04032fcf9e9a5777cc335abc9732
gkolb <gkolb100@gmail.com>
2018-05-05 15:10:09 -0600

    Create factories for tests and add testSetup file to run before tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory');
 
 let browswer, page;
 
@@ -26,24 +27,9 @@ test('clicking login starts oauth flow', async () => {
 })
 
 test('When signed in, shows logout button', async () => {
-  const id = '5aeb659c90055f2ca83778e4';
-
-  const Buffer = require('safe-buffer').Buffer;
-  const sessionObject = {
-    passport: {
-      user: id
-    }
-  };
-  const sessionString = Buffer.from(
-    JSON.stringify(sessionObject)
-  ).toString('base64');
-
-  const Keygrip = require('keygrip');
-  const keys = require('../config/keys');
-  const keygrip = new Keygrip([keys.cookieKey]);
-  const sig = keygrip.sign('session=' + sessionString);
+  const { session, sig } = sessionFactory();
   
-  await page.setCookie({ name: 'session', value: sessionString});
+  await page.setCookie({ name: 'session', value: session});
   await page.setCookie({ name: 'session.sig', value: sig});
   await page.goto('localhost:3000');
   await page.waitFor('a[href="/auth/logout"]');
