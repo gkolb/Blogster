85a82aec0503e1fe179cd35ea1ce611bf63af666
gkolb <gkolb100@gmail.com>
2018-05-07 14:48:53 -0600

    Finish all setup for Travis CI build

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://gkolb:123@ds113358.mlab.com:13358/blog_dev_node',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
