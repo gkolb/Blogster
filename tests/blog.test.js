4ffbeec96d3dff6e5e4a3e221956c85d7f7919cf
gkolb <gkolb100@gmail.com>
2018-05-07 10:51:04 -0600

    Add test for valid input on blog post

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -23,10 +23,29 @@ describe('When logged in', async () => {
     expect(label).toEqual('Blog Title');
   });
 
+  describe('And using valid input', async () => {
+    beforeEach(async () => {
+      await page.type('.title input', 'My Title');
+      await page.type('.content input', 'My Content');
+      await page.click('form button');
+    });
+
+    test('Submitting takes user to review screen', async () => {
+      const text = await page.getContentsOf('h5');
+
+      expect(text).toEqual('Please confirm your entries');
+    });
+
+    test('Submitting then saving adds blog to index page', async () => {
+
+    });
+  })
+
   describe('And using invalid inputs', async () => {
     beforeEach(async () => {
       await page.click('form button');
     });
+
     test('the form shows an error message', async () => {
       const titleError = await page.getContentsOf('.title .red-text');
       const contentError = await page.getContentsOf('.content .red-text');
